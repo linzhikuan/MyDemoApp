e9609210e041bc7efc28a37fcff6da6996b135ea
linzhikuan <1065627939@qq.com>
2025-12-07 10:09:33 +0800

    新增mmkv的组件

---
+++ core/mmkv/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-parcelize")
+}
+
+// 应用通用库配置
+apply(from = "${rootProject.projectDir}/gradles/library.gradle")
+
+android {
+    namespace = "com.lzk.core.mmkv"
+}
+
+dependencies {
+    // MMKV - 高性能键值存储
+    implementation("com.tencent:mmkv:1.3.9")
+}
